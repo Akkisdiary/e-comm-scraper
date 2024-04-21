b5e0bc35ecddcda60a127f5b93cf88091610123a
Akshay Shegaonkar <67048783+Akkisdiary@users.noreply.github.com>
2024-04-21 06:05:19 +0000

    implement details extraction

---
+++ bots/amazoncom/constants.py
@@ -0,0 +1,150 @@
+TITLE_PATHS = (
+    '//div[@id="aod-asin-title"]/h5//text()',
+    '//h1[@id="title"]/span[@id="productTitle"]/text()',
+    '//div[@id="title_feature_div"]//h1[@id="title"]/text()',
+    '//span[contains(@id, "btAsinTitle")]/span/text()',
+    '//span[contains(@id, "btAsinTitle")]/text()',
+    '//span[contains(@id, "ebooksProductTitle")]/text()',
+    '//div[contains(@class, "dpSubDetailProductTitle")]/text()',
+    '//h1[@id="title"]/text()',
+    '//span[@id="title"]/text()',
+    '//table[contains(@class, "layout")]' '//span[contains(@class, "title")]/text()',
+    '//div[@id="mediaProductTitle_feature' '_div"]//span[@id="productTitle"]/text()',
+    '//b[@id="product-title"]/text()',
+    '//div[contains(@class, "olpNavBackTo") '
+    'and contains(@class, "hideOnSecondaryView")]//text()',
+    '//div[contains(@class, "olp-text")]/span/text()',
+    '//div[@id="olpProductDetails"]/h1/text()[last()]',
+    '//h1[@data-automation-id="title"]/text()',
+    '//div[contains(@id, "ProductTitle")]/h1/text()',
+)
+
+AVAILABILITY_PATHS = (
+    '//span[@class="availGreen" or @class="availRed"]/text()',
+    '//div[@id="availability"]/span/text()',
+    '//span[@id="availability"]/text()',
+    '//span[@id="pantry-availability"]/text()',
+    '//div[contains(@id,"Availability_feature")]/div/*/text()',
+    '//div[@id="availability"]/span//text()',
+)
+
+PRICE_PATHS = [
+    '//div[contains(@id, "price_feature_div")]//span['
+    'contains(@id, "saleprice")]/text()',
+    '//table[contains(@class, "product")]//span['
+    'contains(@id, "actualPriceValue")]/b/text()',
+    '//div[contains(@class, "mbcContainer")]//span['
+    'contains(@class, "price")]/text()',
+    '//span[contains(@id, "ourprice")]/text()',
+    '//div[contains(@id, "buybox")]//div['
+    'contains(@id, "buyNewSection")]//span['
+    'contains(@class, "offer-price")]/text()',
+    '//span[contains(@id, "priceblock_ourprice")]/text()',
+    '//div[contains(@id, "price_feature_div")]//span['
+    'contains(@id, "dealprice")]/text()',
+    '//div[@id="newOfferAccordionRow"]//span[contains('
+    '@class, "header-price")]/text()',
+    '//div[contains(@id, "MultiOfferEgress_feature")]//span[contains('
+    '@class, "a-color-price")]/text()',
+    '//div[@class="sub-priceBlock"]//span[contains(@class, "a-color-price")]/text()',
+    '//div[@id="cerberus-data-metrics"]//@data-asin-price',
+    '//div[contains(@id, "Price_feature_div") or contains(@id,"corePrice_desktop")]'
+    '//*[contains(@id,"ourprice")]/*[contains(@class,"color-price")]/text()',
+    '//div[contains(@id, "newAccordionRow")]//div[contains(@id, "Price_feature_div")]//span[@class="a-offscreen"]/text()',
+    '//div[contains(@id, "Price_feature_div") or contains(@id,"corePrice_desktop")]//*[not(ancestor-or-self::div[contains(@id,"usedAccordionRow")])]'
+    '//*[@data-a-color="price" or @data-a-color="base"]/*[contains(@class,"a-offscreen")]/text()',
+    '//span[contains(@class,"reinventPricePriceToPayMargin priceToPay")]/span[@class="a-offscreen"]/text()',
+    '//span[contains(@class,"a-color-price priceBlockBuyingPriceString")]/span[contains(@class,"a-color-price")]/text()',
+]
+
+ASIN_PATHS = (
+    '//form[contains(@id, "addToCart")]//input[@name="ASIN"]/@value',
+    '//input[@name="ASIN"]/@value',
+    '//input[contains(@name, "metric-asin")][1]/@name[1]',
+    '//input[@name="a"]/@value',
+    '//form[contains(@name, "kindle-order")]/input[@name="ASIN.0"]/@value',
+    '//a[@id="olpDetailPageLink"]/@href',
+    '//div[contains(@class, "olpNavBackTo")]/a/@href',
+    '//span[contains(@class, "a-text-bold") and contains(text(), "ASIN")]'
+    "/following-sibling::span/text()",
+    '//span[contains(@class, "asinReviewsSummary")]/@name',
+    '//div[@id="dynamicDeliveryMessage" or @id="deliveryBlockMessage"]/parent::'
+    '*/input[contains(@id, "SelectAsin")]/@value',
+)
+
+ASIN_REGEXES = (
+    r"/(B[\d][\w]{8})(?:[^\w-]|$)",
+    r"/gp/aw/ol/(\w+)/?",
+    r"/gp/offer-listing/(\w+)/?",
+    r"/dp/(\w+)/?",
+    r"/dp/product-listing/(\w+)/?",
+    r"/gp/aw/d/(\w+)/?",
+    r"[\?&]?asin=(\w+)&?",
+    r"/gp/product/(\w+)/?",
+)
+
+SELLER_NAME_PATHS = [
+    '//div[@id="tabular-buybox"]//tr//span[contains(text(),"Sold by")]'
+    '/ancestor::td/ancestor::tr//span[contains(@class,"truncate-full")]/'
+    'span[contains(@class,"tabular-buybox-text")]//text()',
+    '//div[@class="buying" and contains(., "sold by")]//b/text()',
+    '//*[@id="merchant-info" and contains(./text(),"sold by")]//text()',
+    '//div[@id="merchant-info" and contains(., "Sold by")]/a/text()',
+    '//div[@id="desktop_qualifiedBuyBox"]//div[@class="tabular-buybox-text" and @tabular-attribute-name="Sold by"]//text()',
+    '//div[contains(@class,"tabular-buybox")]/div[contains(@class,"buybox-text") '
+    'and @tabular-attribute-name="Sold by"]//text()',
+    '//*[contains(@id,"desktop_qualifiedBuyBox")]//*[contains(text(), "Sold by")]/parent::div/following-sibling::div/div/span//text()',
+    '//div[@id="merchant-info"]//text()',
+    '//div[@id="fresh-merchant-info"]//text()',
+    '//*[@id="merchant-info"]//text()',
+    '//a[contains(@class, "MerchantName")]/text()',
+    '//div[contains(@class, "buying")]/b/a/text()',
+    '//div[contains(@id, "pantry-availability")]/text()',
+    '//div[@id="buybox-tabular"]//tr//span[contains(text(),"Sold by")]'
+    '/ancestor::td/ancestor::tr//span[contains(@class,"truncate-full")]//text()',
+    '//*[contains(text(), "Sold by")]/parent::td/following-sibling::td/*/text()',
+]
+
+
+REGULAR_PRICE_PATHS = (
+    '//span[contains(@id, "listPriceValue")]/text()',
+    '//td[contains(text(), "List Price")]'
+    '/following-sibling::td[contains(@class, "a-text-strike")]/text()',
+    '//span[contains(@class, "listprice")]/text()',
+    '//td[@class="listprice"]/text()',
+    '//td[contains(text(), "List Price")]/following-sibling::td//text()',
+    '//div[@id="corePriceDisplay_desktop_feature_div"]//span[contains(@class,"price") and @data-a-strike="true"]//span[@class="a-offscreen"]/text()',
+    '//div[contains(@id, "kindle-price-block")]'
+    '//span[contains(@class, "listPrice")]/text()',
+    '//div[@id="newOfferAccordionRow"]//span[contains(@class, "a-text-strike")]/text()',
+    '//div[@id="price"]//span[contains(@class, "a-text-strike")]/text()',
+    '//span[contains(@class, "priceBlockStrikePriceString")]/text()',
+    '//div[contains(@id,"corePrice_desktop")]//*[@data-a-color="secondary"]/*[contains(@class,"a-offscreen")]/text()',
+    '//div[@id="newAccordionRow_0"]//span[@data-a-color="price"]/span[@class="a-offscreen"]/text()',
+    '//div[@id="newAccordionRow_0"]//span[@data-a-color="secondary"]/span[@class="a-offscreen"]/text()',
+    '//div[contains(@id, "Price_feature_div")]//*[@data-a-color="price"]/*[contains(@class,"a-offscreen")]/text()',
+    '//div[contains(@id, "Price_feature_div")]//*[@data-a-color="secondary"]/*[contains(@class,"a-offscreen")]/text()',
+    '//span[contains(@class,"basisPrice") and contains(text(),"Suggested price")]//span[contains(@class,"a-offscreen")]/text()',
+    '//div[@id="qualifiedBuybox"]//div[@id="corePrice_feature_div"]//span[@class="a-offscreen"]/text()',
+    '//div[@id="ppd"]//div[contains(@id,"corePriceDisplay")]//span[@data-a-strike="true"]//span[@class="a-offscreen" ]//text()',
+    '//div[contains(@id, "buybox")]' '//span[contains(@class, "a-text-strike")]/text()',
+)
+
+
+BRAND_PATHS = (
+    '//a[contains(@id, "brand")]/text()',
+    '//div[@id="mbc"]/@data-brand',
+    '//span[@id="brand"]/text()',
+    '//a[@id="bylineInfo"]/text()',
+    'translate(substring-after(substring-before(//a[contains(@id, "brand")]/@href, "/b/"),"https://www.amazon.com/"),"-"," ")',
+)
+
+IMAGE_PATHS = (
+    '//td[@id="prodImageCell"]/a/img/@src',
+    '//img[contains(@id, "main-image")]/@data-hires-replacement',
+    '//img[contains(@id, "main-image")]/@src',
+    '//div[@id="avod-main"]//img[@id="prod-img"]/@src',
+    '//div[@id="rwImages_hidden"]/img/@src',
+    '//span[@data-action="thumb-action"]//img/@src',
+    '//div[contains(@class, "imgTagWrapper")]/img/@data-old-hires',
+)
---
+++ bots/amazoncom/details.py
@@ -0,0 +1,140 @@
+"""Extract details from amazon.com products from a given csv file"""
+
+import re
+import sys
+from datetime import datetime
+from typing import Dict, List, Type, Any
+
+import pandas as pd
+from furl import furl
+from lxml import html
+from lxml.html import HtmlElement
+
+import web
+from utils import export, xpath
+from bots.amazoncom import constants
+
+
+def read_urls_from_csv(filepath: str):
+    df = pd.read_csv(filepath)
+    if "url" not in df.columns:
+        raise ValueError("The given csv file does not contain urls")
+
+    return df["url"].to_list()
+
+
+def get_asin(doc: Type[HtmlElement], url: str):
+    def _run_asin_regexes(value):
+        for regex in constants.ASIN_REGEXES:
+            asin = re.findall(regex, value)
+            if asin:
+                return asin[0]
+
+    asin = None
+
+    if doc is not None:
+        asin = xpath.extract(doc, constants.ASIN_PATHS, 0)
+        if doc.xpath('//script[contains(. ,\'"parentAsin":"{0}\')]'.format(asin)):
+            # don't allow the parent asin to be the asin
+            asin = None
+
+    if not asin and url:
+        asin = _run_asin_regexes(url)
+
+    if asin and any(u in asin for u in ("/gp/", "/dp/")):
+        asin = _run_asin_regexes(asin)
+
+    if not asin:
+        return None
+
+    return asin.split(".")[-1].strip()
+
+
+def validate_response(response: web.Response):
+    response.raise_for_status()
+
+
+def get_product_response(url: str):
+    url = furl(url)
+    headers = {
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'referer': 'https://www.amazon.com/',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    }
+    headers = web.random_headers(**headers)
+
+    return web.get(url.url, headers=headers)
+
+
+def extract_product_details(html_content: str, url: str):
+    result = {}
+    doc = html.fromstring(html_content)
+
+    result["title"] = xpath.extract(doc, constants.TITLE_PATHS, 0)
+    result["brand"] = xpath.extract(doc, constants.BRAND_PATHS, 0)
+    result["price"] = xpath.extract(doc, constants.PRICE_PATHS, 0)
+    result["list_price"] = xpath.extract(doc, constants.REGULAR_PRICE_PATHS, 0)
+    result["image"] = xpath.extract(doc, constants.IMAGE_PATHS, 0)
+    result["sku"] = get_asin(doc, url)
+
+    return result
+
+
+def extract(urls: List[str]):
+    data = []
+    
+    for url in urls:
+        response = get_product_response(url)
+        validate_response(response)
+
+        details = extract_product_details(response.content, url)
+        data.append(details)
+
+    return data
+
+
+def clean_and_transform(raw_data: List[Dict[str, Any]]):
+    schema = {
+        "title": [],
+        "brand": [],
+        "price": [],
+        "list_price": [],
+        "image": [],
+        "sku": [],
+    }
+    for data in raw_data:
+        for key in schema.keys():
+            val = data.get(key)
+            if val is not None:
+                val=  str(val).strip()
+            schema[key].append(val)
+
+    return schema
+
+
+def main(urls: List[str]):
+    print(f"Reading csv: '{filepath}'")
+    urls = read_urls_from_csv(filepath)
+
+    print(f"Extrating product information ({len(urls)} urls)")
+    raw = extract(urls)
+    final = clean_and_transform(raw)
+
+    file_name = f"amazoncom-details-{datetime.now().isoformat(timespec='seconds')}"
+    print(f"Exporting to file: {file_name}")
+    export.to_csv(final, file_name)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        raise ValueError("Please enter a csv file path")
+    
+    filepath = sys.argv[1]
+    main(filepath)
+    
+    print("===COMPLETE===")
